37a49b45ec8830ba3a3002c035077a9d9c50ca87
Дмитрий Тарасов <demiat@mail.ru>
2024-11-27 23:12:47 +0300

    dev

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Category, IceCream, Topping, Wrapper
 
-# Register your models here.
+admin.site.empty_value_display = 'Не задано'
+
+
+@admin.register(IceCream)
+class IceCreamAdmin(admin.ModelAdmin):
+
+    @staticmethod
+    @admin.display(description='Топинги')
+    def show_toppings(obj):
+        return ', '.join(topping.title for topping in obj.toppings.all())
+
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper',
+        'show_toppings'
+    )
+    # readonly_fields = ('toppings',)
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category', 'wrapper')
+    list_display_links = ('title',)
+    filter_horizontal = ('toppings',)
+
+
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Wrapper)
+admin.site.register(Topping)
+# admin.site.register(IceCream, IceCreamAdmin)
--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -5,17 +5,34 @@ from core.models import PublishedModel
 
 class Category(PublishedModel):
     title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    slug = models.SlugField('Слаг', max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 
 class Topping(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title = models.CharField(
+        'Обертка',
+        max_length=256,
+        help_text='Уникальное название обёртки, не более 256 символов'
+    )
+
+    def __str__(self):
+        return self.title
 
 
 class IceCream(PublishedModel):
@@ -27,12 +44,20 @@ class IceCream(PublishedModel):
         related_name='ice_cream',
         null=True,
         blank=True,
+        verbose_name='Обертка'
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
+        verbose_name='Категория'
     )
     toppings = models.ManyToManyField(Topping)
     is_on_main = models.BooleanField(default=False)
 
+    class Meta:
+        verbose_name = 'Мороженое'
+        verbose_name_plural = 'Мороженое'
+
+    def __str__(self):
+        return self.title
