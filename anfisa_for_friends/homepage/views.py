aa9dac0b44ca0d86a26692c060ac5960f733f132
tdv <demiat@mail.ru>
2024-11-28 17:03:49 +0300

    next dev

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    # Запрос:
+    ice_cream_list = IceCream.objects.values('title').exclude(is_published=False).filter(is_on_main=True)
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template, context)
